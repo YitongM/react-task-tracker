e8d2de4165ebc4dfb58fd41866b0e2bd0548a1e2
Yitong <yitong.mao@hotmail.fr>
2021-02-18 23:03:40 +0100

    Add virtual backend server with Json Server

--- src/App.js
+++ src/App.js
@@ -1,40 +1,53 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
 import AddTask from "./components/AddTask"
 
 function App() {
 	const [showAddTask, setShowAddTask] = useState(false)
-	const [tasks, setTasks] = useState([
-		{
-			id: 1,
-			text: 'Doctor',
-			day: '2021.2.1 @ 14:30',
-			reminder: true
-		},
-		{
-			id: 2,
-			text: 'Meeting',
-			day: '2021.2.2 @ 14:30',
-			reminder: false
-		},
-		{
-			id: 3,
-			text: 'Food',
-			day: '2021.2.3 @ 14:30',
-			reminder: true
-		}
-	])
+	const [tasks, setTasks] = useState([])
 
+	useEffect(() => {
+		const getTasks = async () => {
+			const tasksFromServer = await fetchTasks()
+			setTasks(tasksFromServer)
+		}
+		
+		getTasks()
+	}, [])
+	
+	// Fetch data
+	const fetchTasks = async () => {
+		const res = await fetch('http://localhost:5000/tasks')
+		const data = await res.json()
+
+		return data
+	}
+	
 	// Add
-	const addTask = (task) => {
-		const id = Math.floor(Math.random() * 10000) + 1
-		const newTask = { id, ...task }
-		setTasks([...tasks, newTask])
+	const addTask = async (task) => {
+		const res = await fetch('http://localhost:5000/tasks', {
+			method: 'POST',
+			header: {
+				'Content-type': 'application/json'
+			},
+			body: JSON.stringify(task)
+		})
+
+		const data = await res.json()
+		setTasks([...tasks, data])
+		
+		// const id = Math.floor(Math.random() * 10000) + 1
+		// const newTask = { id, ...task }
+		// setTasks([...tasks, newTask])
 	}
 
 	// Delete a task
-	const deleteTask = (id) => {
+	const deleteTask = async (id) => {
+		await fetch(`http://localhost:5000/tasks/${id}`, {
+			method: 'DELETE'
+		})
+		
 		setTasks(tasks.filter((task) => task.id != id))
 	}
 
